d2cae8d1ae4902f32363b82be0c2bfa712a09ad0
fuxiangke <252300663@qq.com>
2026-03-16 16:19:54 +0800

    升级静态页面设计

--- data_manage/app/main.py
+++ data_manage/app/main.py
@@ -4,28 +4,20 @@ from fastapi.templating import Jinja2Templates
 from app.routers import upload, query, download
 from app.db.init_db import init_db
 
-app = FastAPI(
-    title="Task Data Service",
-    version="1.0"
-)
+app = FastAPI(title="Task Data Service")
 
-# 初始化数据库
+templates = Jinja2Templates(directory="app/templates")
+
 @app.on_event("startup")
 def startup():
     init_db()
 
-# 注册API路由
 app.include_router(upload.router)
 app.include_router(query.router)
 app.include_router(download.router)
 
-# 注册模板目录
-templates = Jinja2Templates(directory="app/templates")
-
-
-# 首页
 @app.get("/")
-def home(request: Request):
+def index(request: Request):
     return templates.TemplateResponse(
         "index.html",
         {"request": request}
